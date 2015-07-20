03f74ac0df9c3ae43e965d18b4e00fdd87376c2b
Dan Persa <dan.persa@gmail.com>
2015-07-20 22:55:15 +0200

    Removed basic auth from the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :authorize
 
-  http_basic_authenticate_with :name => "frodo", :password => "thering"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
