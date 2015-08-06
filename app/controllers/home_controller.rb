b872d1daea10bd338b87d5739ffa45b34f0f034d
Dan Persa <dan.persa@gmail.com>
2015-08-06 12:20:46 +0200

    Added metadata to the home pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,32 @@
 class HomeController < BasicAuthController
+
+  @@default_title = 'Remindmetolive - Photos and Stories'
+  @@default_keywords = 'photography, photos, story, stories, photographs, narratives, share, human, street, travel, cats'
+
+  @@index_meta ||= PostMeta.new title: "#{@@default_title}",
+                                description: 'At Remindmetolive, we combine photography with storytelling. Let us help you shape and share your story with the world!',
+                                keywords: "#{@@default_keywords}"
+
+  @@contact_meta ||= PostMeta.new title: "#{@@default_title} - Contact Us",
+                                description: 'Contact the Remindmetolive team. Let us help you shape and share your story with the world!',
+                                keywords: "contact, #{@@default_keywords}"
+
+  @@about_meta ||= PostMeta.new title: "#{@@default_title} - About Us",
+                                description: 'We started the Remindmetolive project to share stories of people and places through photography. Let us help you shape and share your story with the world!',
+                                keywords: "about, #{@@default_keywords}"
   def index
+    @meta = @@index_meta
   end
 
   def contact
+    @meta = @@contact_meta
+  end
+
+  def about
+    @meta = @@about_meta
   end
 
   def send_contact
     ContactMailer.contact(params[:contact][:name], params[:contact][:email], params[:contact][:subject], params[:contact][:message]).deliver_now
-    # render 'send_contact'
-  end
-
-  def about
   end
 end
